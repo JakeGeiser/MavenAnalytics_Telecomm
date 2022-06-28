11e3dc5be88ae1d6d49e8928a129e5d62622302c
Jake Geiser <geiser.jt@gmail.com>
2022-06-27 23:00:16 -0500

    initialize

---
+++ src/main/java/com/mavenanalytics/telecom/MavenAnalyticsTelecommApplication.java
@@ -0,0 +1,13 @@
+package com.mavenanalytics.telecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenAnalyticsTelecommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenAnalyticsTelecommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mavenanalytics/telecom/MavenAnalyticsTelecommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavenanalytics.telecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenAnalyticsTelecommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
